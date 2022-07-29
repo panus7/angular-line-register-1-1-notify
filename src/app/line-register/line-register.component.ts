ec09534456ae6760aa8267e3f6ca26742a8eaa9b
panus7 <dev.pnz7@gmail.com>
2022-07-29 11:49:54 +0700

    2 files modified

--- src/app/line-register/line-register.component.ts
+++ src/app/line-register/line-register.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-line-register',
   templateUrl: './line-register.component.html',
-  styleUrls: ['./line-register.component.css']
+  styleUrls: ['./line-register.component.css'],
 })
 export class LineRegisterComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  LineRegister() {
+    //https://notify-bot.line.me/my/services/edit?clientId=UxGd8nKydrBVlTaJx3sR99
+    this.router.navigate([
+      'https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=v8B5HhU9qgwFCj8leuNDeQ&redirect_uri=http://localhost/linecallback&scope=notify&state=sitecall',
+    ]);
   }
-
 }
